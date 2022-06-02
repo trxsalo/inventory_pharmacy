1d37638dd1730fac30d51477f65649b9f2f9a969
trxsalome <marcosarauz46@gmail.com>
2022-06-01 20:24:22 -0400

    se crearon los formularios base los y las routas.get

--- src/routes/links.js
+++ src/routes/links.js
@@ -1,10 +1,45 @@
+
 const express = require("express");
 const router = express.Router();
 
-//const conn = require("../database");
+///// ----- GET ----------  /////
 
-router.get("/add", (res,req) =>{
-    res.render("../views/links/add.hbs");
+
+//Formulario para agregar una nueva presentaación
+router.get("/addPresentacion", (req, res) =>{
+    res.render("../views/links/form/addPresentacioForm");
 });
+
+//Formulario para agregar un nuevo Grupo Terapeutico
+router.get("/addGrupoTerapeutico", (req, res) =>{
+    res.render("../views/links/form/addGrupoTeraForm");
+});
+
+//Formulario para agregar un nuevo Medicamento
+router.get("/addMedicamento", (req, res) =>{
+    res.render("../views/links/form/addMedicamentoForm");
+});
+
+//Formulario para agregar un nuevo Medicamento
+router.get("/addLote", (req, res) =>{
+    res.render("../views/links/form/addLoteForm");
+});
+
+
+//Formulario para agregar un nuevo laboratorio
+router.get("/addLaboratorio", (req, res) =>{
+    res.render("../views/links/form/addLaboratorioForm");
+});
+
+
+
+///// ----- POST ----------  /////
+
+//ENVIAR EL DATOS DE GRUPO TERAPEUTICO
+router.post("/addGrupoTerapeutico", (req, res) =>{
+    res.send("recivido");
+});
+
+
 
 module.exports = router;
